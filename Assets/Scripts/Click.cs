5339d6a310abd95ec1c2f83f3dcf7ee665c93afd
Casimir Kuusela <crk.kuusela@gmail.com>
2017-03-27 17:59:28 +0300

    First push

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text dropDisplay;
+    public double drops = 0;
+    public double dropsperclick = 1;
+    public double droprate = 0.5;
+    public int interval = 1;
+    float nexttime = 0;
+
+
+	// Update is called once per frame
+	void Update () {
+
+        dropDisplay.text = "Drops: " + drops;
+
+        if (Time.time >= nexttime)
+        {
+
+            drops -= droprate;
+
+            nexttime += interval;
+
+        }
+        
+
+    }
+
+
+    public void Touched()
+    {
+
+        drops += dropsperclick;
+        
+    }
+
+ 
+
+    
+
+}
