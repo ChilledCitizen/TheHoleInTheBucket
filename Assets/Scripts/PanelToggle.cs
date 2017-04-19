6ea78b4bea3f8a63fbeb48492d2d25570c943645
Lemurface <dr621@hotmail.com>
2017-04-19 18:43:55 +0300

    Added PanelToggle, Added ShakeOnClick

---
+++ Assets/Scripts/PanelToggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelToggle : MonoBehaviour
+{
+	[Tooltip("The object (container) to toggle.")]
+	public GameObject Target;
+
+	[Tooltip("Toggle key")]
+	public string ToggleKey = "b";
+
+	[Tooltip("Display on startup?")]
+	public bool Visible = false;
+
+
+	void Start()
+	{
+		// Set panel's default visibility.
+		Target.SetActive(Visible);
+	}
+
+	void Update()
+	{
+		// Check key
+		if (Input.GetKeyDown(ToggleKey))
+		{
+			// Toggle
+			if (Visible == true)
+			{
+				Visible = false;
+				Target.SetActive(Visible);
+			}
+			else
+			{
+				Visible = true;
+				Target.SetActive(Visible);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ShakeOnClick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeOnClick : MonoBehaviour
+{
+	[Tooltip("The object (collider/container) to shake")]
+	public GameObject make_shake;
+
+	[Tooltip("Maximum tilt = angle_range/2")]
+	public int angle_range = 30;
+
+	[Tooltip("Seconds to shake")]
+	public double shake_duration = 0.5;
+	double shake_duration_current;
+	bool shaking = false;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (shaking)
+		{
+			// while there's still time left..
+			if ((shake_duration_current -= Time.deltaTime) > float.Epsilon)
+			{
+				// set a random angle.
+				float random = Random.value;
+				float angle = (angle_range * random) - (angle_range / 2);
+
+				make_shake.transform.eulerAngles = new Vector3(0, 0, angle);
+			}
+			else
+			{
+				// set angle back to normal.
+				make_shake.transform.eulerAngles = Vector3.zero;
+				shaking = false;
+			}
+		}
+	}
+
+	void OnMouseDown()
+	{
+		StartShaking();
+	}
+
+	public void StartShaking()
+	{
+		shaking = true;
+		shake_duration_current = shake_duration;
+	}
+}
